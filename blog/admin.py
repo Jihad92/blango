d5811e8c78c7f3eca1a5671b5317ab46f17c9e52
Jihad Dayoub <pythoner1991@gmail.com>
2023-02-06 21:35:42 +0300

    Generics added

--- blog/admin.py
+++ blog/admin.py
@@ -3,17 +3,45 @@ from django.forms import Textarea, TextInput
 from django.db import models
 from .models import (
     Tag,
-    Post
+    Post,
+    Comment
 )
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 # Register your models here.
-
 class TagAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.CharField: {
             'widget': TextInput(
                 attrs={
                     'size': 100
+                }
+            )
+        }
+    }
+
+class CommentAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {
+            'widget': Textarea(
+                attrs={
+                    'rows': 8,
+                    'cols': 100,
+                    'style': 'resize: none'
+                }
+            )
+        }
+    }
+
+class CommentInline(GenericTabularInline):
+    model = Comment
+    formfield_overrides = {
+        models.TextField: {
+            'widget': Textarea(
+                attrs={
+                    'rows': 8,
+                    'cols': 100,
+                    'style': 'resize: none'
                 }
             )
         }
@@ -42,6 +70,10 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('title',),
     }
+    inlines = [
+        CommentInline,
+    ]
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.conf import settings
 
@@ -9,6 +11,20 @@ class Tag(models.Model):
     def __str__(self):
         return self.value
 
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.creator}.comment.{self.id}'
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     title = models.CharField(max_length=100)
@@ -16,7 +32,8 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     
-    tags = models.ManyToManyField(Tag, related_name='posts', null=True)
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    comments = GenericRelation(Comment)
     
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
